aff7fc812db6e6e1dd8dd33dd27a963e8be1be79
Patrick <pmartindesigns@gmail.com>
2019-03-14 01:17:57 -0700

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -786,7 +786,7 @@ $settings['entity_update_batch_size'] = 50;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'rmssss',
+  'database' => 'rms-app',
   'username' => 'root',
   'password' => 'root',
   'prefix' => '',
